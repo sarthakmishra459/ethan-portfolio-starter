9f83e67b5f139c0f1ac5a0f8de3d1396b193a6f7
Sarthak Mishra <124512392+sarthakmishra459@users.noreply.github.com>
2023-09-10 22:17:43 +0530

    Portfolio Website

--- tailwind.config.js
+++ tailwind.config.js
@@ -24,7 +24,7 @@ module.exports = {
       colors: {
         primary: '#131424',
         secondary: '#393A47',
-        accent: '#F13024',
+        accent: '#24f127',
       },
       backgroundImage: {
         explosion: 'url("/bg-explosion.png")',
